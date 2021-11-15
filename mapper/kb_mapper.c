bacf1a0189280e80457dda4398bf569abf9b4d6e
Constantinos Argyriou <constarg@pm.me>
2021-11-15 14:43:46 +0200

    Make a few basic things - kb_mapper

--- mapper/kb_mapper.c
+++ mapper/kb_mapper.c
@@ -15,14 +15,23 @@
 #define DEVICE_HANDLER_PATH     "/dev/input/"
 #define KEYBOARD_ID             "EV=120013"
 
+static int latest_worker_id = 0;
 
 static void create_kb_worker(const char *kb_event_file) {
-    // TODO - Here make a new thread and call the reader.
+    // TODO - Here make a new thread and call the worker.
 }
 
 static int has_kb_worker(const char *kb_event_file) {
     // TODO - Here find if the keyboard we are looking for had already a worker, so we don't make duplicate workers.
     return 0;
+}
+
+static char **parse_devices() {
+    return NULL;
+}
+
+static char **retrieve_kb_handlers(const char **parsed_devices) {
+    return NULL;
 }
 
 void map_keyboards(void *arg) {
--- mem/include/mem.h
+++ mem/include/mem.h
@@ -1,6 +1,17 @@
 #ifndef LINUX_KEYLOGGER_MEM_H
 #define LINUX_KEYLOGGER_MEM_H
 
+#include <malloc.h>
+
+#define ALLOC_MEM(VAR, SIZE, SIZE_OF_TYPE) {                \
+        VAR = calloc(SIZE, SIZE_OF_TYPE);                   \
+        if (VAR == NULL) exit(1);                           \
+} while(0)
+
+#define REALLOC_MEM(VAR, NEW_SIZE, SIZE_OF_TYPE) {          \
+        VAR = realloc(VAR, NEW_SIZE * SIZE_OF_TYPE);        \
+        if (VAR == NULL) exit(1);                           \
+} while(0)
 
 
 
