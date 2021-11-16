2fcb17bea430e916429dd2a471bae2ba288a350f
Constantinos Argyriou <rounnus@gmail.com>
2021-11-16 23:45:29 +0200

    Fix a few things.

--- mapper/include/kb_mapper.h
+++ mapper/include/kb_mapper.h
@@ -7,17 +7,6 @@
 
 typedef int8_t status;
 
-#define KB_WORKER_UNKNOWN 2
-#define KB_WORKER_RUNNING 1
-#define KB_WORKER_FAILED  0
-
-struct kb_worker {
-    pthread_t *kb_thread;             // The associated thread for the keyboard.
-    char      *kb_event_file;         // The event file of the keyboard.
-    int        kb_id;                 // The id of the current keyboard worker.
-    status     kb_status: 3;          // The status of the keyboard worker.
-};
-
 extern void map_keyboards();
 
 #endif
--- worker/include/kb_worker.h
+++ worker/include/kb_worker.h
@@ -4,6 +4,18 @@
 
 #include <kb_mapper.h>
 
+#define KB_WORKER_UNKNOWN 2
+#define KB_WORKER_RUNNING 1
+#define KB_WORKER_FAILED  0
+
+struct kb_worker {
+    pthread_t *kb_thread;             // The associated thread for the keyboard.
+    char      *kb_event_file;         // The event file of the keyboard.
+    int        kb_id;                 // The id of the current keyboard worker.
+    status     kb_status: 3;          // The status of the keyboard worker.
+};
+
+
 _Noreturn extern void *start_worker(void *worker);
 
 
