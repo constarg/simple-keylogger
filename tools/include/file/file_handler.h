403cd05b90568fc9d1edb703427b99d90570689c
Constantinos Argyriou <constarg@pm.me>
2021-11-17 14:11:37 +0200

    Almost done

--- tools/include/file/file_handler.h
+++ tools/include/file/file_handler.h
@@ -2,7 +2,11 @@
 #ifndef LINUX_KEYLOGGER_FILE_HANDLER_H
 #define LINUX_KEYLOGGER_FILE_HANDLER_H
 
+#include <stddef.h>
 
+extern void create_log_file(int kb_id);
+
+extern void append_to_file(const char **keystroke_buffer, size_t keystroke_buffer_s, int kb_id);
 
 
 #endif
