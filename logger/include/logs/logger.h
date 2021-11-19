051f1e21cbba4adc98f07e1c139b73888d9c7901
Constantinos Argyriou <constarg@pm.me>
2021-11-19 22:54:46 +0200

    Add comments.

--- logger/include/logs/logger.h
+++ logger/include/logs/logger.h
@@ -6,6 +6,11 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * Build a log message and print it in on the opened terminal.
+ * @param message The message to be printed.
+ * @param kb_id The keyboard that the message is related to.
+ */
 static void inline make_terminal_log(const char *message, int kb_id) {
     char *kb_msg = "Keyboard-";
     char output[strlen(message) + strlen(kb_msg) + 5];
@@ -14,6 +19,12 @@ static void inline make_terminal_log(const char *message, int kb_id) {
     printf("%s\n",output);
 }
 
+/**
+ * Build a log about the captured keystroke ( in real time ) and print
+ * it in the terminal.
+ * @param keystroke The keystroke that has been captured.
+ * @param kb_id The keyboard from where the keystroke comes from.
+ */
 static void inline make_keystroke_log(const char *keystroke, int kb_id) {
     char *basic_msg = "Pressed key has been captured:";
     char message[strlen(basic_msg) + strlen(keystroke)];
--- tools/include/file/file_handler.h
+++ tools/include/file/file_handler.h
@@ -4,8 +4,19 @@
 
 #include <stddef.h>
 
+/**
+ * Creates a unique file for each keyboard that
+ * is connected in the system and writ's there the keystrokes
+ * that it capture.
+ * @param kb_id The related keyboard.
+ */
 extern void create_log_file(int kb_id);
 
+/**
+ * Write in the end of the log file the captured keystroke.
+ * @param keystroke_buffer The decoded keystroke.
+ * @param kb_id The related keyboard.
+ */
 extern void append_to_file(const char *keystroke_buffer, int kb_id);
 
 
